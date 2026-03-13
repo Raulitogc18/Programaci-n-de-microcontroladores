8acb14bf2e950f473acca92a41e7e3a7e3a3bb6c
Raulitogc18 <152231063+Raulitogc18@users.noreply.github.com>
2026-03-12 20:45:02 -0600

    Add files via upload

---
+++ Ejercicio en clase/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Include/core_cm4.h \
+ C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Include/cmsis_version.h \
+ C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Include/mpu_armv7.h \
+ C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Include/core_cm4.h:
+C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Include/cmsis_version.h:
+C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Include/cmsis_compiler.h:
+C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Include/cmsis_gcc.h:
+C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Include/mpu_armv7.h:
+C:/Users/raulg/STM32CubeIDE/workspace_1.19.0/CMSIS/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
